05c85b4add07f9ae8f86c1988ed07938036fd23f
mraduljaiswani <mradul.jaiswani@gmail.com>
2023-11-08 14:11:05 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { Todo } from "../../Todo";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mradul-todo-list';
+  constructor(){}
 }
